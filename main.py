e388c6c70fb5d4a692abddebccf139ea675b72c8
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-11-22 21:27:16 -0500

    feat: implemented area for rectangle

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+import math
+
+def rectangle_area(length, width):
+    if length < 0 or width < 0:
+        raise ValueError("Length and width must be non-negative.")
+    return length * width
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def test_should_return_area():
     assert rectangle_area(5, 10) == 50
     assert rectangle_area(0, 10) == 0
